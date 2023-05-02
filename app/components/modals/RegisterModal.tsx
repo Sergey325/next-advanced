27b993974e8d4bfa27b0b191984046b5c786b7de
Sergey325 <sabserj2017@gmail.com>
2023-05-02 14:26:35 +0200

    toaster

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -13,6 +13,8 @@ import useRegisterModal from "@/app/hooks/useRegisterModal";
 import Modal from "@/app/components/modals/Modal";
 import Heading from "@/app/components/Heading";
 import Input from "@/app/components/inputs/Input";
+import toast from "react-hot-toast";
+import Button from "@/app/components/Button";
 
 type Props = {
 
@@ -44,7 +46,7 @@ const RegisterModal = (props: Props) => {
             registerModal.onClose()
             })
             .catch((error) => {
-                console.log(error)
+                toast.error("Something went wrong.")
             })
             .finally(() => {
                 setIsLoading(false)
@@ -66,6 +68,54 @@ const RegisterModal = (props: Props) => {
                 errors={errors}
                 required
             />
+            <Input
+                id="name"
+                label='Name'
+                disabled={isLoading}
+                register={register}
+                errors={errors}
+                required
+            />
+            <Input
+                id="password"
+                type="password"
+                label='Password'
+                disabled={isLoading}
+                register={register}
+                errors={errors}
+                required
+            />
+        </div>
+    )
+
+    const footerContent = (
+        <div className="flex flex-col gap-4 mt-3">
+            <hr/>
+            <Button
+                outline
+                label="Continue with Google"
+                icon={FcGoogle}
+                onClick={() => {}}
+            />
+            <Button
+                outline
+                label="Continue with Github"
+                icon={AiFillGithub}
+                onClick={() => {}}
+            />
+            <div className="text-neutral-500 text-center mt-4 font-light">
+                <div className="justify-center flex flex-row items-center gap-2">
+                    <div>
+                        Already have an account?
+                    </div>
+                    <div
+                        onClick={registerModal.onClose}
+                        className="text-neutral-800 cursor-pointer hover:underline"
+                    >
+                        Log in
+                    </div>
+                </div>
+            </div>
         </div>
     )
 
@@ -78,6 +128,7 @@ const RegisterModal = (props: Props) => {
             onClose={registerModal.onClose}
             onSubmit={handleSubmit(onSubmit)}
             body={bodyContent}
+            footer={footerContent}
         />
     );
 };
