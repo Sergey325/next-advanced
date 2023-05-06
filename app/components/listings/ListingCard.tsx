ecccef919488e303b66db49225b43453870163f2
Sergey325 <sabserj2017@gmail.com>
2023-05-06 16:16:13 +0200

    listings

---
+++ app/components/listings/ListingCard.tsx
@@ -0,0 +1,17 @@
+'use client'
+
+import {Listing} from "@prisma/client";
+
+type Props = {
+    data: Listing
+};
+
+const ListingCard = ({data}: Props) => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default ListingCard;
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,47 @@
-export default function Home() {
-  return (
-    <div className="text-rose-500 text-2xl">Hello</div>
-  )
+import ClientOnly from "@/app/components/ClientOnly";
+import Container from "@/app/components/Container";
+import EmptyState from "@/app/components/EmptyState";
+import getListings from "@/app/actions/getListings";
+import ListingCard from "@/app/components/listings/ListingCard";
+
+export default async function Home() {
+    const listings = await getListings()
+    const isEmpty = true
+
+    if(!listings){
+        return (
+            <ClientOnly>
+                <EmptyState showReset/>
+            </ClientOnly>
+        )
+    }
+
+    return (
+        <ClientOnly>
+            <Container>
+                <div
+                    className="
+                        pt-24
+                        grid
+                        grid-cols-1
+                        sm:grid-cols-2
+                        md:grid-cols-3
+                        lg:grid-cols-4
+                        xl:grid-cols-5
+                        2xl:grid-cols-6
+                        gap-8
+                    "
+                >
+                    {listings.map(listing => {
+                        return (
+                            <ListingCard
+                                key={listing.id}
+                                data={listing}
+                            />
+                        )
+                    })}
+                </div>
+            </Container>
+        </ClientOnly>
+    )
 }
