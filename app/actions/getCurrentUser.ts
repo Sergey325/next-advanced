f4cea08becf7525c4c67ec1a1e1884f180330d9d
Serhey325 <sabserj2017@gmail.com>
2023-06-27 22:59:16 +0200

    refactoring for deployment

--- app/actions/getCurrentUser.ts
+++ app/actions/getCurrentUser.ts
@@ -1,5 +1,5 @@
 import {getServerSession} from "next-auth/next";
-import {authOptions} from "@/pages/api/auth/[...nextauth]";
+import {authOptions} from "@/pages/api/auth/[...nextauth]"
 import prisma from "@/app/libs/prismadb"
 
 export async function getSession() {
@@ -8,7 +8,7 @@ export async function getSession() {
 
 export default async function getCurrentUser() {
     try {
-        const session = await getSession()
+        const session  = await getSession()
 
         if(!session?.user?.email){
             return null
